26c0facdcc69c6a66783f26cf7dc7c96ac7efb92
Chulsu Park <chulsupark@gmail.com>
2011-09-19 02:50:16 -0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-#  
-get "users/new"
+#  get "users/new"
 #  get "pages/home"
 #  get "pages/contact"
 #  get "pages/about"
 
+	resources :users
 
   match '/signup', :to => 'users#new'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110917091154) do
+ActiveRecord::Schema.define(:version => 20110919053220) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20110917091154) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
