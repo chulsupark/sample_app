9026c95ee6a2ba63bac5380ffeadb974a95ea136
Chulsu Park <chulsupark@gmail.com>
2011-09-21 03:35:36 -0400

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
-  	def new
-  		@title = "Sign up"
-  	end
-
+    
+    def new
+        @user = User.new
+        @title = "Sign up"
+    end
+    
 	def show
 		@user = User.find(params[:id])
 		@title = @user.name
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe UsersController do
 	render_views
-	
 	  
 	describe "GET 'show'" do
 
@@ -37,15 +36,15 @@ describe UsersController do
   	end
   
   	describe "GET 'new'" do
-    	it "should be successful" do
-      		get 'new'
-      		response.should be_success
-    	end
-    
-    	it "should have the right title" do
-      		get 'new'
-      		response.should have_selector("title", :content => "Sign up")
-    	end
+        it "should be successful" do
+            get :new
+            response.should be_success
+        end
+
+        it "should have the right title" do
+            get :new
+            response.should have_selector("title", :content => "Sign up")
+        end
   	end
 
 end
